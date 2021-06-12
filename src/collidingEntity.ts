a83c53612656e6e9e7e61d4fe750e260ba91dda0
unsafecast <unsafecast@protonmail.com>
2021-06-12 12:32:12 +0300

    added a warning for collision

--- src/collidingEntity.ts
+++ src/collidingEntity.ts
@@ -8,6 +8,9 @@ export class CollidingEntity extends Entity {
     }
     
     collided(entity: Entity) {
+	// WARNING: This assumes the player moves 5px per frame. NEEDS TO CHANGE!!!
+
+	
 	this.x -= 5;
 	if (this.collidesWith(entity)) {
 	    this.x += 10;
